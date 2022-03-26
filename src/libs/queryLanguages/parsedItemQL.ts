e205b5c64526472d689f6ec7975ebecfc20e6d7e
drchain <phvietan@gmail.com>
2022-03-26 23:56:32 +0800

    add query for packet ID

--- src/libs/queryLanguages/parsedItemQL.ts
+++ src/libs/queryLanguages/parsedItemQL.ts
@@ -9,6 +9,11 @@ export class ParsedItemQL<T extends MongoItem> extends QueryLanguage<T> {
     return this;
   }
 
+  public async queryRequestPacketId(requestPacketId: string) {
+    const item = await this.db.getOne({requestPacketId});
+    return item;
+  }
+
   public async setQueryDone(item: T): Promise<void> {
     await this.checkDoneInitDatabase();
     await this.db.update({_id: item._id}, {
